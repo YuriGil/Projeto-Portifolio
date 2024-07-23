60b8fcb9c1a8c373fe5c21b76a87f4eeff7b7f46
Yuri G <yurigink@gmail.com>
2024-07-23 02:50:33 -0300

    atualização GitHub Pages

--- JS/portifolio.js
+++ JS/portifolio.js
@@ -1,38 +1,34 @@
 function updatePortfolio(profileData) {
-    const portfolio = document.getElementById('slideshow')
-    portfolio.innerHTML = profileData.portfolio.map(project => {
-        return `
-<div class="slideshow-container">
-  <div class="project-container mySlides fade" style="display:block">
-    <div class="portifolio-container">
-    <div class="backgroundSlide"></div>
-        <div class="project-image">
-          <img src="${project.portifolioImg}" class="printImage">  
-            <button href="${project.url}" class="visit-site">visitar site</button>
-        </div>
-        <div class="project-details">
-          <h1 ${project.github ? 'class="github"' : ''}>${project.name}</h1>
-            <p>${project.description}</p>
-            <button href="${project.url}" target="_blank">${project.url}ver projeto</button>
-        </div>
-        <div class="text">${project.portifolioNumber}</div>
+  const portfolio = document.getElementById('slideshow');
+  portfolio.innerHTML = profileData.portfolio.map((project, index) => {
+      return `
+      <div class="slideshow-container">
+          <div class="project-container mySlides fade" style="display:${index === 0 ? 'block' : 'none'}">
+              <div class="portifolio-container">
+                  <div class="backgroundSlide"></div>
+                  <div class="project-image">
+                      <img src="${project.portifolioImg}" class="printImage">
+                      <button href="${project.urlDirect}" class="visit-site" type="button">visitar site</button>
+                  </div>
+                  <div class="project-details">
+                      <h1 ${project.github ? 'class="github"' : ''}>${project.name}</h1>
+                      <p>${project.description}</p>
+                      <button href="${project.urlGit}" class="view-project" type="button">ver projeto</button>
+                  </div>
+                  <div class="text">${project.portifolioNumber}</div>
+              </div>
+              <!-- Next and previous buttons -->
+              <a class="prev" onclick="plusSlides(-1)">❮</a>
+              <a class="next" onclick="plusSlides(1)">❯</a>
+          </div>
       </div>
-  <!-- Next and previous buttons -->
-  <a class="prev" onclick="plusSlides(-1)">❮</a>
-  <a class="next" onclick="plusSlides(1)">❯</a>
-</div>
-</div>
+  `;
+  }).join('');
 
-<br>
-
-<!-- The dots/circles -->
-<div>
-  <span class="dot" onclick="currentSlide(1)"></span>
-  <span class="dot" onclick="currentSlide(2)"></span>
-  <span class="dot" onclick="currentSlide(3)"></span>
-</div>
-</section>
-       
-`
-    }).join('')
-  }
+  // Adcionando os botões por fora para não ficar duplicado
+  portfolio.innerHTML += `
+  <div style="text-align:center">
+      ${profileData.portfolio.map((_, index) => `<span class="dot" onclick="currentSlide(${index + 1})"></span>`).join('')}
+  </div>
+  `;
+}
